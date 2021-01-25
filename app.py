999ffc954008594ef215d3cc1258ea4965c3656e
Jon Jackson <ocskier@gmail.com>
2021-01-25 17:20:29 -0500

    feat: add select from closest match

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import json
 import difflib
-from difflib import SequenceMatcher
+from difflib import get_close_matches, SequenceMatcher
 
 data = json.load(open('data.json'))
 word = input('What word would you like defined? ')
 
 def getDefinition(str):
     lowerStr = str.lower()
-    matches = []
     for key in data:
-        if SequenceMatcher(None, lowerStr, key).ratio() > 0.78:
-            matches.append({key : data[key]})
+        if SequenceMatcher(None, lowerStr, key).ratio() == 1.00:
+            return {key: data[key]}
     
+    matches = get_close_matches(lowerStr, data.keys(),5,0.78)
     if len(matches) == 0:
-        return 'Word does not exist!'
+        return 'Can\'t find a matching word!'
     else: 
-        return matches
+        newInput = input(f'Which word closest matches your yours? {matches} ? ')
+        return getDefinition(newInput)
 
-for i in getDefinition(word):
-    print(json.dumps(i, indent=2))
+print(json.dumps(getDefinition(word), indent=2))
